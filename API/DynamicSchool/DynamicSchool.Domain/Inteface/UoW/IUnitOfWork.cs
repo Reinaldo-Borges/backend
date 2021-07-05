ca594d46a7a1413eb1817a6a75f5c4657bcaae73
Reinaldo-Borges <vital.rborges@terceiros.mag.com.br>
2021-07-05 00:07:22 -0300

    infra.data

---
+++ API/DynamicSchool/DynamicSchool.Domain/Inteface/UoW/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DynamicSchool.Domain.Inteface.UoW
+{
+    public interface IUnitOfWork
+    {
+        Task Commit();
+    }
+}
---
+++ API/DynamicSchool/DynamicSchool.Infra.Data/infrastructure/UoW/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DynamicSchool.Domain.Inteface.UoW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSchool.Infra.Data.infrastructure.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public Task Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
